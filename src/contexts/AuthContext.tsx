0aa7411cbd7edfc10286462ad832dc1f18304399
bhunos <bhuno.souzaa@gmail.com>
2022-02-25 16:26:43 -0300

    erros na integracao

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,17 +1,29 @@
 import { createContext, useEffect, useState } from "react";
 import Router from "next/router";
+import axios from "axios";
+import { response } from "express";
+
+type SignInData = {
+  email: string;
+  password: string;
+};
+
+type User = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 type AuthContextType = {
   isAuthenticated: boolean;
-  user: { user: any; signIn: () => Promise<{ err: unknown } | undefined> };
-  signIn: any;
+  user: User;
+  signIn: (data: SignInData) => Promise<void>;
 };
 
 export const AuthContext = createContext({} as AuthContextType);
 
 export function AuthProvider({ children }: any) {
-  const [user, setUser] = useState<any | any>(null);
-
+  const [user, setUser] = useState<User | any>(null);
   async function signIn() {
     try {
       const user = await fetch(`https://api.agapeoc.com.br`, {
@@ -24,6 +36,9 @@ export function AuthProvider({ children }: any) {
       }).then((response) => {
         console.log(response);
       });
+      console.log(user);
+
+      console.log("entrou");
     } catch (err) {
       return { err };
     }
